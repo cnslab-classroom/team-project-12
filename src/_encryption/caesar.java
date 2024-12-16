b818714597d717bbdc51d161d3e8129a097ea987
andy6368 <andy6368@naver.com>
2024-12-16 21:15:30 +0900

    카이사르 암호화 클래스(임병우)

---
+++ src/_encryption/caesar.java
@@ -0,0 +1,39 @@
+package _encryption;
+
+public class caesar{
+    private static int shift;
+    
+    public caesar(int shift) {
+        this.shift = shift % 95;
+    }
+
+    public static String encrypt(String plainText) {
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (char c : plainText.toCharArray()) {
+            if (c >= 32 && c <= 126) {
+                char encryptedChar = (char) ((c - 32 + shift) % 95 + 32);
+                encryptedText.append(encryptedChar);
+            } else {
+                encryptedText.append(c);
+            }
+        }
+
+        return encryptedText.toString();
+    }
+
+    public static String decrypt(String encryptedText) {
+        StringBuilder decryptedText = new StringBuilder();
+
+        for (char c : encryptedText.toCharArray()) {
+            if (c >= 32 && c <= 126) {
+                char decryptedChar = (char) ((c - 32 - shift + 95) % 95 + 32);
+                decryptedText.append(decryptedChar);
+            } else {
+                decryptedText.append(c);
+            }
+        }
+
+        return decryptedText.toString();
+    }
+}
